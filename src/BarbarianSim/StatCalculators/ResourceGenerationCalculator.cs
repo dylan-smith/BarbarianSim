8e5442bd95bd54f2529cbd6fc7caee6f00774d1a
Dylan Smith <dylan-smith@github.com>
2023-07-04 10:24:15 -0500

    added resource generation bonus for willpower

---
+++ src/BarbarianSim/StatCalculators/ResourceGenerationCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace BarbarianSim.StatCalculators;
+
+public class ResourceGenerationCalculator : BaseStatCalculator
+{
+    public static double Calculate(SimulationState state) => Calculate<ResourceGenerationCalculator>(state);
+
+    protected override double InstanceCalculate(SimulationState state)
+    {
+        var resourceGeneration = state.Config.Gear.GetStatTotal(g => g.ResourceGeneration);
+        resourceGeneration += WillpowerCalculator.Calculate(state) * 0.03;
+
+        return 1.0 + (resourceGeneration / 100.0);
+    }
+}
