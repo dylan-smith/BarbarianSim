955dc5063daf123d5c3d6501015055f5b54d28d1
Dylan Smith <dylan-smith@github.com>
2023-07-04 09:23:07 -0500

    Added dexterity calculator and tests

---
+++ src/BarbarianSim.Tests/StatCalculators/DexterityCalculatorTests.cs
@@ -0,0 +1,59 @@
+﻿using BarbarianSim.Config;
+using BarbarianSim.StatCalculators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarbarianSim.Tests.StatCalculators;
+
+[TestClass]
+public class DexterityCalculatorTests
+{
+    [TestMethod]
+    public void Includes_Base_Value()
+    {
+        var config = new SimulationConfig();
+        config.PlayerSettings.Level = 1;
+        var state = new SimulationState(config);
+
+        var result = DexterityCalculator.Calculate(state);
+
+        Assert.AreEqual(8, result);
+    }
+
+    [TestMethod]
+    public void Includes_Dexterity_Gear_Bonus()
+    {
+        var config = new SimulationConfig();
+        config.PlayerSettings.Level = 1;
+        config.Gear.Helm.Dexterity = 42;
+        var state = new SimulationState(config);
+
+        var result = DexterityCalculator.Calculate(state);
+
+        Assert.AreEqual(50, result);
+    }
+
+    [TestMethod]
+    public void Includes_All_Stats_Gear_Bonus()
+    {
+        var config = new SimulationConfig();
+        config.PlayerSettings.Level = 1;
+        config.Gear.Helm.AllStats = 17;
+        var state = new SimulationState(config);
+
+        var result = DexterityCalculator.Calculate(state);
+
+        Assert.AreEqual(25, result);
+    }
+
+    [TestMethod]
+    public void Includes_Level_Bonus()
+    {
+        var config = new SimulationConfig();
+        config.PlayerSettings.Level = 100;
+        var state = new SimulationState(config);
+
+        var result = DexterityCalculator.Calculate(state);
+
+        Assert.AreEqual(107, result);
+    }
+}
---
+++ src/BarbarianSim.Tests/StatCalculators/DodgeCalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using BarbarianSim.Config;
+using BarbarianSim.StatCalculators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarbarianSim.Tests.StatCalculators;
+
+[TestClass]
+public class DodgeCalculatorTests
+{
+    [TestCleanup]
+    public void TestCleanup() => BaseStatCalculator.ClearMocks();
+
+    [TestMethod]
+    public void Returns_0_By_Default()
+    {
+        var state = new SimulationState(new SimulationConfig());
+        BaseStatCalculator.InjectMock(typeof(DexterityCalculator), new FakeStatCalculator(0.0));
+
+        var result = DodgeCalculator.Calculate(state);
+
+        Assert.AreEqual(0.0, result);
+    }
+
+    [TestMethod]
+    public void Includes_Dodge_Gear_Bonus()
+    {
+        var config = new SimulationConfig();
+        config.Gear.Helm.Dodge = 42;
+        var state = new SimulationState(config);
+        BaseStatCalculator.InjectMock(typeof(DexterityCalculator), new FakeStatCalculator(0.0));
+
+        var result = DodgeCalculator.Calculate(state);
+
+        Assert.AreEqual(0.42, result);
+    }
+
+    [TestMethod]
+    public void Includes_Dexterity_Bonus()
+    {
+        var state = new SimulationState(new SimulationConfig());
+        BaseStatCalculator.InjectMock(typeof(DexterityCalculator), new FakeStatCalculator(400.0));
+
+        var result = DodgeCalculator.Calculate(state);
+
+        Assert.AreEqual(0.04, result);
+    }
+}
