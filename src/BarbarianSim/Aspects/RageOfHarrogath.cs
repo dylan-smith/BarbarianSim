43f565c58d3d250388a8d8d2d7c7610bdd391263
Dylan Smith <dylan-smith@github.com>
2023-08-07 21:51:05 -0500

    comment

--- src/BarbarianSim/Aspects/RageOfHarrogath.cs
+++ src/BarbarianSim/Aspects/RageOfHarrogath.cs
@@ -4,5 +4,7 @@ namespace BarbarianSim.Aspects;
 
 public class RageOfHarrogath : Aspect
 {
+    // Lucky Hit: Up to a [20 - 40]% chance to reduce the Cooldowns of your Non-Ultimate Skills by 1.5 seconds when you inflict Bleeding on Elites.
     public int Chance { get; set; }
+    public const double COOLDOWN_REDUCTION = 1.5;
 }
