bd8a609c67160603694977ddcb757c95fd8e6154
Dylan Smith <dylan-smith@github.com>
2023-08-08 21:06:03 -0500

    logging for GutteralYellProcEventHandler

--- src/BarbarianSim/EventHandlers/GutteralYellProcEventHandler.cs
+++ src/BarbarianSim/EventHandlers/GutteralYellProcEventHandler.cs
@@ -6,9 +6,14 @@ namespace BarbarianSim.EventHandlers;
 
 public class GutteralYellProcEventHandler : EventHandler<GutteralYellProcEvent>
 {
+    public GutteralYellProcEventHandler(SimLogger log) => _log = log;
+
+    private readonly SimLogger _log;
+
     public override void ProcessEvent(GutteralYellProcEvent e, SimulationState state)
     {
         e.GutteralYellAuraAppliedEvent = new AuraAppliedEvent(e.Timestamp, "Gutteral Yell", GutteralYell.DURATION, Aura.GutteralYell);
         state.Events.Add(e.GutteralYellAuraAppliedEvent);
+        _log.Verbose($"Created AuraAppliedEvent for Gutteral Yell for {GutteralYell.DURATION} seconds");
     }
 }
