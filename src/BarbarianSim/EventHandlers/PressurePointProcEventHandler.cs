4f95bd8778af91ba810f59f8d71dda3847453f9a
Dylan Smith <dylan-smith@github.com>
2023-08-08 22:06:47 -0500

    logging for PressurePointProcEventHandler

--- src/BarbarianSim/EventHandlers/PressurePointProcEventHandler.cs
+++ src/BarbarianSim/EventHandlers/PressurePointProcEventHandler.cs
@@ -6,10 +6,14 @@ namespace BarbarianSim.EventHandlers;
 
 public class PressurePointProcEventHandler : EventHandler<PressurePointProcEvent>
 {
+    public PressurePointProcEventHandler(SimLogger log) => _log = log;
+
+    private readonly SimLogger _log;
+
     public override void ProcessEvent(PressurePointProcEvent e, SimulationState state)
     {
         e.VulnerableAppliedEvent = new AuraAppliedEvent(e.Timestamp, "Pressure Point", PressurePoint.VULNERABLE_DURATION, Aura.Vulnerable, e.Target);
-
         state.Events.Add(e.VulnerableAppliedEvent);
+        _log.Verbose($"Created AuraAppliedEvent for Vulnerable on Enemy #{e.Target.Id} for {PressurePoint.VULNERABLE_DURATION:F2} seconds");
     }
 }
