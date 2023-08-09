3ebf11f3a99789a8de7437b6b7f202ee5cd97673
Dylan Smith <dylan-smith@github.com>
2023-08-08 19:44:45 -0500

    logging tweaks

--- src/BarbarianSim/StatCalculators/ResourceGenerationCalculator.cs
+++ src/BarbarianSim/StatCalculators/ResourceGenerationCalculator.cs
@@ -35,44 +35,39 @@ public class ResourceGenerationCalculator
         if (configBonus > 0)
         {
             resourceGeneration += configBonus;
-            _log.Verbose($"Resource Generation bonus from Gear/Paragon = {resourceGeneration}%");
+            _log.Verbose($"Resource Generation bonus from Gear/Paragon = {resourceGeneration:F2}%");
         }
 
         var willpowerBonus = _willpowerCalculator.Calculate(state) * 0.03;
         if (willpowerBonus > 0)
         {
             resourceGeneration += willpowerBonus;
-            _log.Verbose($"Resource Generation bonus from Willpower = {willpowerBonus}%");
+            _log.Verbose($"Resource Generation bonus from Willpower = {willpowerBonus:F2}%");
         }
 
         var result = 1.0 + (resourceGeneration / 100.0);
 
-        var rallyingCryBonus = _rallyingCry.GetResourceGeneration(state);
-        if (rallyingCryBonus > 1)
-        {
-            result *= rallyingCryBonus;
-            _log.Verbose($"Resource Generation bonus from Rallying Cry = {rallyingCryBonus}x");
-        }
+        result *= _rallyingCry.GetResourceGeneration(state);
 
         var tacticalRallyingCryBonus = _tacticalRallyingCry.GetResourceGeneration(state);
         if (tacticalRallyingCryBonus > 1)
         {
             result *= tacticalRallyingCryBonus;
-            _log.Verbose($"Resource Generation bonus from Tactical Rallying Cry = {tacticalRallyingCryBonus}x");
+            _log.Verbose($"Resource Generation bonus from Tactical Rallying Cry = {tacticalRallyingCryBonus:F2}x");
         }
 
         var prolificFuryBonus = _prolificFury.GetFuryGeneration(state);
         if (prolificFuryBonus > 1)
         {
             result *= prolificFuryBonus;
-            _log.Verbose($"Resource Generation bonus from Prolific Fury = {prolificFuryBonus}x");
+            _log.Verbose($"Resource Generation bonus from Prolific Fury = {prolificFuryBonus:F2}x");
         }
 
         var primeWrathOfTheBerserkerBonus = _primeWrathOfTheBerserker.GetResourceGeneration(state);
         if (primeWrathOfTheBerserkerBonus > 1)
         {
             result *= primeWrathOfTheBerserkerBonus;
-            _log.Verbose($"Resource Generation bonus from Prime Wrath of the Berserker = {primeWrathOfTheBerserkerBonus}x");
+            _log.Verbose($"Resource Generation bonus from Prime Wrath of the Berserker = {primeWrathOfTheBerserkerBonus:F2}x");
         }
 
         _log.Verbose($"Resource Generation multiplier = {result:F2}x");
