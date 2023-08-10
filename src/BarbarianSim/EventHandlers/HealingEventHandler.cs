8fe1a0e86c483983d55da99d33bad4a67b9d93fc
Dylan Smith <dylan-smith@github.com>
2023-08-10 00:47:47 -0500

    log formatting

--- src/BarbarianSim/EventHandlers/HealingEventHandler.cs
+++ src/BarbarianSim/EventHandlers/HealingEventHandler.cs
@@ -20,18 +20,18 @@ public class HealingEventHandler : EventHandler<HealingEvent>
     {
         _log.Verbose($"Base Amount Healed = {e.BaseAmountHealed:F2}");
         e.AmountHealed = e.BaseAmountHealed * _healingReceivedCalculator.Calculate(state);
-        _log.Verbose($"Total Healing = {e.AmountHealed}");
+        _log.Verbose($"Total Healing = {e.AmountHealed:F2}");
 
         var maxLife = _maxLifeCalculator.Calculate(state);
         if (e.AmountHealed + state.Player.Life > maxLife)
         {
             e.OverHeal = state.Player.Life + e.AmountHealed - maxLife;
-            _log.Verbose($"Overheal = {e.OverHeal}");
+            _log.Verbose($"Overheal = {e.OverHeal:F2}");
             e.AmountHealed -= e.OverHeal;
-            _log.Verbose($"Actual Amount Healed = {e.AmountHealed}");
+            _log.Verbose($"Actual Amount Healed = {e.AmountHealed:F2}");
         }
 
         state.Player.Life += e.AmountHealed;
-        _log.Verbose($"New Player Life = {state.Player.Life}");
+        _log.Verbose($"New Player Life = {state.Player.Life:F2}");
     }
 }
