ece9d8a871e3eb615cdc64996e3963ba1d691127
Dylan Smith <dylan-smith@github.com>
2023-08-10 00:00:12 -0500

    logging for TemperedFury

--- src/BarbarianSim/Skills/TemperedFury.cs
+++ src/BarbarianSim/Skills/TemperedFury.cs
@@ -5,22 +5,27 @@ namespace BarbarianSim.Skills;
 public class TemperedFury
 {
     // Increase your Maximum Fury by X
+    public TemperedFury(SimLogger log) => _log = log;
+
+    private readonly SimLogger _log;
 
     public virtual double GetMaximumFury(SimulationState state)
     {
-        var skillPoints = 0;
+        var skillPoints = state.Config.GetSkillPoints(Skill.TemperedFury);
 
-        if (state.Config.Skills.ContainsKey(Skill.TemperedFury))
-        {
-            skillPoints += state.Config.Skills[Skill.TemperedFury];
-        }
-
-        return skillPoints switch
+        var result = skillPoints switch
         {
             1 => 3,
             2 => 6,
             >= 3 => 9,
             _ => 0,
         };
+
+        if (result > 0)
+        {
+            _log.Verbose($"Maximum Fury Bonus from Tempered Fury = {result:F2} with {skillPoints} skill points");
+        }
+
+        return result;
     }
 }
