abc2e1d4c0ff41daf75c4532232c598fb9bf7cca
Dylan Smith <dylan-smith@github.com>
2023-08-09 23:58:26 -0500

    logging from TacticalRallyingCry

--- src/BarbarianSim/Skills/TacticalRallyingCry.cs
+++ src/BarbarianSim/Skills/TacticalRallyingCry.cs
@@ -9,19 +9,28 @@ public class TacticalRallyingCry : IHandlesEvent<RallyingCryEvent>
     public const double RESOURCE_GENERATION = 1.20;
     public const double FURY_GENERATED = 20.0;
 
+    public TacticalRallyingCry(SimLogger log) => _log = log;
+
+    private readonly SimLogger _log;
+
     public void ProcessEvent(RallyingCryEvent e, SimulationState state)
     {
         if (state.Config.HasSkill(Skill.TacticalRallyingCry))
         {
             state.Events.Add(new FuryGeneratedEvent(e.Timestamp, "Tactical Rallying Cry", FURY_GENERATED));
+            _log.Verbose($"Tactical Rallying Cry created FuryGeneratedEvent for {FURY_GENERATED:F2} Fury");
         }
     }
 
     public virtual double GetResourceGeneration(SimulationState state)
     {
-        return state.Config.HasSkill(Skill.TacticalRallyingCry) &&
-               state.Player.Auras.Contains(Aura.RallyingCry)
-            ? RESOURCE_GENERATION
-            : 1.0;
+        if (state.Config.HasSkill(Skill.TacticalRallyingCry) &&
+               state.Player.Auras.Contains(Aura.RallyingCry))
+        {
+            _log.Verbose($"Resource Generation from Tactical Rallying Cry = {RESOURCE_GENERATION}x");
+            return RESOURCE_GENERATION;
+        }
+
+        return 1.0;
     }
 }
