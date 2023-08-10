94fcabca2eadf065a222e2fc8cca5a6a02f3c2ba
Dylan Smith <dylan-smith@github.com>
2023-08-09 21:30:23 -0500

    logging for EnhancedRallyingCry

--- src/BarbarianSim.Tests/Skills/EnhancedRallyingCryTests.cs
+++ src/BarbarianSim.Tests/Skills/EnhancedRallyingCryTests.cs
@@ -3,14 +3,18 @@ using BarbarianSim.Enums;
 using BarbarianSim.Events;
 using BarbarianSim.Skills;
 using FluentAssertions;
+using Moq;
 using Xunit;
 
 namespace BarbarianSim.Tests.Skills;
 
 public class EnhancedRallyingCryTests
 {
+    private readonly Mock<SimLogger> _mockSimLogger = TestHelpers.CreateMock<SimLogger>();
     private readonly SimulationState _state = new(new SimulationConfig());
-    private readonly EnhancedRallyingCry _skill = new();
+    private readonly EnhancedRallyingCry _skill;
+
+    public EnhancedRallyingCryTests() => _skill = new(_mockSimLogger.Object);
 
     [Fact]
     public void Creates_UnstoppableAuraAppliedEvent()
--- src/BarbarianSim/Skills/EnhancedRallyingCry.cs
+++ src/BarbarianSim/Skills/EnhancedRallyingCry.cs
@@ -6,11 +6,16 @@ namespace BarbarianSim.Skills;
 public class EnhancedRallyingCry : IHandlesEvent<RallyingCryEvent>
 {
     // Rallying Cry grants you Unstoppable while active
+    public EnhancedRallyingCry(SimLogger log) => _log = log;
+
+    private readonly SimLogger _log;
+
     public void ProcessEvent(RallyingCryEvent e, SimulationState state)
     {
-        if (state.Config.Skills.ContainsKey(Skill.EnhancedRallyingCry) && state.Config.Skills[Skill.EnhancedRallyingCry] > 0)
+        if (state.Config.GetSkillPoints(Skill.EnhancedRallyingCry) > 0)
         {
             state.Events.Add(new AuraAppliedEvent(e.Timestamp, "Enhanced Rallying Cry", e.Duration, Aura.Unstoppable));
+            _log.Verbose($"Enhanced Rallying Cry created AuraAppliedEvent for Unstoppable for {e.Duration:F2} seconds");
         }
     }
 }
