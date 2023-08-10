b2818995838bffb6515360394584b29f222501c8
Dylan Smith <dylan-smith@github.com>
2023-08-09 22:14:55 -0500

    logging for GutteralYell

--- src/BarbarianSim/Skills/GutteralYell.cs
+++ src/BarbarianSim/Skills/GutteralYell.cs
@@ -8,22 +8,42 @@ public class GutteralYell : IHandlesEvent<WarCryEvent>, IHandlesEvent<Challengin
     // Your Shout skills cause enemies to deal X% less damage for 5 seconds
     public const double DURATION = 5;
 
-    public void ProcessEvent(WarCryEvent e, SimulationState state) => state.Events.Add(new GutteralYellProcEvent(e.Timestamp));
+    public GutteralYell(SimLogger log) => _log = log;
 
-    public void ProcessEvent(ChallengingShoutEvent e, SimulationState state) => state.Events.Add(new GutteralYellProcEvent(e.Timestamp));
+    private readonly SimLogger _log;
 
-    public void ProcessEvent(RallyingCryEvent e, SimulationState state) => state.Events.Add(new GutteralYellProcEvent(e.Timestamp));
+    public void ProcessEvent(WarCryEvent e, SimulationState state)
+    {
+        state.Events.Add(new GutteralYellProcEvent(e.Timestamp));
+        _log.Verbose($"Gutteral Yell created GutteralYellProcEvent");
+    }
+
+    public void ProcessEvent(ChallengingShoutEvent e, SimulationState state)
+    {
+        state.Events.Add(new GutteralYellProcEvent(e.Timestamp));
+        _log.Verbose($"Gutteral Yell created GutteralYellProcEvent");
+    }
+
+    public void ProcessEvent(RallyingCryEvent e, SimulationState state)
+    {
+        state.Events.Add(new GutteralYellProcEvent(e.Timestamp));
+        _log.Verbose($"Gutteral Yell created GutteralYellProcEvent");
+    }
 
     public virtual double GetDamageReduction(SimulationState state)
     {
         var skillPoints = state.Config.GetSkillPoints(Skill.GutteralYell);
 
-        return skillPoints switch
+        var result = skillPoints switch
         {
             1 => 4,
             2 => 8,
             >= 3 => 12,
             _ => 0,
         };
+
+        _log.Verbose($"Gutteral Yell reduced damage by {result:F2}%");
+
+        return result;
     }
 }
