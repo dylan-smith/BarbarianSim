89971e50bc25c239218e872f52c9464f1311e9c4
Dylan Smith <dylan-smith@github.com>
2023-08-09 22:39:20 -0500

    logging for InvigoratingFury

--- src/BarbarianSim.Tests/Skills/InvigoratingFuryTests.cs
+++ src/BarbarianSim.Tests/Skills/InvigoratingFuryTests.cs
@@ -12,13 +12,14 @@ namespace BarbarianSim.Tests.Skills;
 public class InvigoratingFuryTests
 {
     private readonly Mock<MaxLifeCalculator> _mockMaxLifeCalculator = TestHelpers.CreateMock<MaxLifeCalculator>();
+    private readonly Mock<SimLogger> _mockSimLogger = TestHelpers.CreateMock<SimLogger>();
     private readonly SimulationState _state = new(new SimulationConfig());
     private readonly InvigoratingFury _skill;
 
     public InvigoratingFuryTests()
     {
         _mockMaxLifeCalculator.Setup(m => m.Calculate(It.IsAny<SimulationState>())).Returns(1200);
-        _skill = new InvigoratingFury(_mockMaxLifeCalculator.Object);
+        _skill = new InvigoratingFury(_mockMaxLifeCalculator.Object, _mockSimLogger.Object);
     }
 
     [Fact]
--- src/BarbarianSim/Skills/InvigoratingFury.cs
+++ src/BarbarianSim/Skills/InvigoratingFury.cs
@@ -7,9 +7,14 @@ namespace BarbarianSim.Skills;
 public class InvigoratingFury : IHandlesEvent<FurySpentEvent>
 {
     // Heal for 3% of your Maximum Life for each 100 Fury spent
-    public InvigoratingFury(MaxLifeCalculator maxLifeCalculator) => _maxLifeCalculator = maxLifeCalculator;
+    public InvigoratingFury(MaxLifeCalculator maxLifeCalculator, SimLogger log)
+    {
+        _maxLifeCalculator = maxLifeCalculator;
+        _log = log;
+    }
 
     private readonly MaxLifeCalculator _maxLifeCalculator;
+    private readonly SimLogger _log;
 
     public void ProcessEvent(FurySpentEvent e, SimulationState state)
     {
@@ -21,26 +26,30 @@ public class InvigoratingFury : IHandlesEvent<FurySpentEvent>
             // but I don't think there's any way to spend more than 100 fury at once
             if (Math.Floor(totalFurySpent / 100) != Math.Floor((totalFurySpent - e.FurySpent) / 100))
             {
-                state.Events.Add(new HealingEvent(e.Timestamp, "Invigorating Fury", _maxLifeCalculator.Calculate(state) * GetHealingPercentage(state)));
+                var healingAmount = _maxLifeCalculator.Calculate(state) * GetHealingPercentage(state);
+                state.Events.Add(new HealingEvent(e.Timestamp, "Invigorating Fury", healingAmount));
+                _log.Verbose($"Invigorating Fury created HealingEvent for {healingAmount:F2}");
             }
         }
     }
 
     public virtual double GetHealingPercentage(SimulationState state)
     {
-        var skillPoints = 0;
+        var skillPoints = state.Config.GetSkillPoints(Skill.InvigoratingFury);
 
-        if (state.Config.Skills.ContainsKey(Skill.InvigoratingFury))
-        {
-            skillPoints += state.Config.Skills[Skill.InvigoratingFury];
-        }
-
-        return skillPoints switch
+        var result = skillPoints switch
         {
             1 => 0.03,
             2 => 0.06,
             >= 3 => 0.09,
             _ => 0,
         };
+
+        if (result > 0)
+        {
+            _log.Verbose($"Healing Percentage from Invigorating Fury = {result:F2}x with {skillPoints} skill points");
+        }
+
+        return result;
     }
 }
