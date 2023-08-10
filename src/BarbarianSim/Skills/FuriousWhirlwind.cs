0dc522087689a44aa830ba54262f72af9cc59e49
Dylan Smith <dylan-smith@github.com>
2023-08-09 22:12:51 -0500

    logging for FuriousWhirlwind

--- src/BarbarianSim/Skills/FuriousWhirlwind.cs
+++ src/BarbarianSim/Skills/FuriousWhirlwind.cs
@@ -9,14 +9,20 @@ public class FuriousWhirlwind : IHandlesEvent<WhirlwindSpinEvent>
     public const double BLEED_DAMAGE = 0.4;
     public const double BLEED_DURATION = 5.0;
 
+    public FuriousWhirlwind(SimLogger log) => _log = log;
+
+    private readonly SimLogger _log;
+
     public void ProcessEvent(WhirlwindSpinEvent e, SimulationState state)
     {
         if (state.Config.Skills.ContainsKey(Skill.FuriousWhirlwind) && state.Config.PlayerSettings.SkillWeapons[Skill.Whirlwind] == state.Config.Gear.TwoHandSlashing)
         {
             foreach (var enemy in state.Enemies)
             {
-                var bleedAppliedEvent = new BleedAppliedEvent(e.Timestamp, "Furious Whirlwind", e.BaseDamage * BLEED_DAMAGE, BLEED_DURATION, enemy);
+                var bleedDamage = e.BaseDamage * BLEED_DAMAGE;
+                var bleedAppliedEvent = new BleedAppliedEvent(e.Timestamp, "Furious Whirlwind", bleedDamage, BLEED_DURATION, enemy);
                 state.Events.Add(bleedAppliedEvent);
+                _log.Verbose($"Furious Whirlwind created BleedAppliedEvent for {bleedDamage:F2} damage over {BLEED_DURATION:F2} seconds on Enemy #{enemy.Id}");
             }
         }
     }
