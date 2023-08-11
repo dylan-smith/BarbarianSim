86ed05a37f60afbea251bb6debaec77be4a8d548
Dylan Smith <dylan-smith@github.com>
2023-08-10 22:31:21 -0500

    logging for IntelligenceCalculator

--- src/BarbarianSim.Tests/StatCalculators/IntelligenceCalculatorTests.cs
+++ src/BarbarianSim.Tests/StatCalculators/IntelligenceCalculatorTests.cs
@@ -1,14 +1,18 @@
 ﻿using BarbarianSim.Config;
 using BarbarianSim.StatCalculators;
 using FluentAssertions;
+using Moq;
 using Xunit;
 
 namespace BarbarianSim.Tests.StatCalculators;
 
 public class IntelligenceCalculatorTests
 {
+    private readonly Mock<SimLogger> _mockSimLogger = TestHelpers.CreateMock<SimLogger>();
     private readonly SimulationState _state = new(new SimulationConfig());
-    private readonly IntelligenceCalculator _calculator = new();
+    private readonly IntelligenceCalculator _calculator;
+
+    public IntelligenceCalculatorTests() => _calculator = new(_mockSimLogger.Object);
 
     [Fact]
     public void Includes_Base_Value()
--- src/BarbarianSim/StatCalculators/IntelligenceCalculator.cs
+++ src/BarbarianSim/StatCalculators/IntelligenceCalculator.cs
@@ -2,13 +2,42 @@
 
 public class IntelligenceCalculator
 {
+    public IntelligenceCalculator(SimLogger log) => _log = log;
+
+    private readonly SimLogger _log;
+
     public virtual double Calculate(SimulationState state)
     {
-        var intelligence = state.Config.GetStatTotal(g => g.Intelligence);
-        intelligence += state.Config.GetStatTotal(g => g.AllStats);
-        intelligence += state.Config.PlayerSettings.Intelligence;
-        intelligence += state.Config.PlayerSettings.Level - 1;
+        var intFromConfig = state.Config.GetStatTotal(g => g.Intelligence);
+        if (intFromConfig > 0)
+        {
+            _log.Verbose($"Intelligence from Config = {intFromConfig:F2}");
+        }
 
-        return intelligence;
+        var intFromAllStats = state.Config.GetStatTotal(g => g.AllStats);
+        if (intFromAllStats > 0)
+        {
+            _log.Verbose($"Intelligence from All Stats = {intFromAllStats:F2}");
+        }
+
+        var baseInt = state.Config.PlayerSettings.Intelligence;
+        if (baseInt > 0)
+        {
+            _log.Verbose($"Base Intelligence = {baseInt:F2}");
+        }
+
+        var intFromLevel = state.Config.PlayerSettings.Level - 1;
+        if (intFromLevel > 0)
+        {
+            _log.Verbose($"Intelligence from Level = {intFromLevel:F2}");
+        }
+
+        var result = intFromConfig + intFromAllStats + baseInt + intFromLevel;
+        if (result > 0)
+        {
+            _log.Verbose($"Total Intelligence = {result:F2}");
+        }
+
+        return result;
     }
 }
