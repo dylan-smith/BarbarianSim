91d60759a6f1b2f556ee8cd6f08174f1d357cc9c
Dylan Smith <dylan-smith@github.com>
2023-08-11 00:31:02 -0500

    logging tweaks for ResourceGenerationCalculator

--- src/BarbarianSim/StatCalculators/ResourceGenerationCalculator.cs
+++ src/BarbarianSim/StatCalculators/ResourceGenerationCalculator.cs
@@ -35,7 +35,7 @@ public class ResourceGenerationCalculator
         if (configBonus > 0)
         {
             resourceGeneration += configBonus;
-            _log.Verbose($"Resource Generation bonus from Gear/Paragon = {resourceGeneration:F2}%");
+            _log.Verbose($"Resource Generation bonus from Config = {resourceGeneration:F2}%");
         }
 
         var willpowerBonus = _willpowerCalculator.Calculate(state) * 0.03;
@@ -48,29 +48,11 @@ public class ResourceGenerationCalculator
         var result = 1.0 + (resourceGeneration / 100.0);
 
         result *= _rallyingCry.GetResourceGeneration(state);
+        result *= _tacticalRallyingCry.GetResourceGeneration(state);
+        result *= _prolificFury.GetFuryGeneration(state);
+        result *= _primeWrathOfTheBerserker.GetResourceGeneration(state);
 
-        var tacticalRallyingCryBonus = _tacticalRallyingCry.GetResourceGeneration(state);
-        if (tacticalRallyingCryBonus > 1)
-        {
-            result *= tacticalRallyingCryBonus;
-            _log.Verbose($"Resource Generation bonus from Tactical Rallying Cry = {tacticalRallyingCryBonus:F2}x");
-        }
-
-        var prolificFuryBonus = _prolificFury.GetFuryGeneration(state);
-        if (prolificFuryBonus > 1)
-        {
-            result *= prolificFuryBonus;
-            _log.Verbose($"Resource Generation bonus from Prolific Fury = {prolificFuryBonus:F2}x");
-        }
-
-        var primeWrathOfTheBerserkerBonus = _primeWrathOfTheBerserker.GetResourceGeneration(state);
-        if (primeWrathOfTheBerserkerBonus > 1)
-        {
-            result *= primeWrathOfTheBerserkerBonus;
-            _log.Verbose($"Resource Generation bonus from Prime Wrath of the Berserker = {primeWrathOfTheBerserkerBonus:F2}x");
-        }
-
-        _log.Verbose($"Resource Generation multiplier = {result:F2}x");
+        _log.Verbose($"Total Resource Generation multiplier = {result:F2}x");
         return result;
     }
 }
