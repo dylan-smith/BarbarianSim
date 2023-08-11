465513917a2943acd5db47ca83cfefa1ec2f7a81
Dylan Smith <dylan-smith@github.com>
2023-08-10 22:09:59 -0500

    dodge logging conditionals

--- src/BarbarianSim/StatCalculators/DodgeCalculator.cs
+++ src/BarbarianSim/StatCalculators/DodgeCalculator.cs
@@ -14,13 +14,22 @@ public class DodgeCalculator
     public virtual double Calculate(SimulationState state)
     {
         var dodgeFromConfig = state.Config.GetStatTotalMultiplied(g => 1 - (g.Dodge / 100.0));
-        _log.Verbose($"Dodge From Config = {1 - dodgeFromConfig:P2}");
+        if (dodgeFromConfig > 0)
+        {
+            _log.Verbose($"Dodge From Config = {1 - dodgeFromConfig:P2}");
+        }
 
         var dodgeFromDexterity = 1 - (_dexterityCalculator.Calculate(state) * 0.01 / 100.0);
-        _log.Verbose($"Dodge From Dexterity = {1 - dodgeFromDexterity:P2}");
+        if (dodgeFromDexterity > 0)
+        {
+            _log.Verbose($"Dodge From Dexterity = {1 - dodgeFromDexterity:P2}");
+        }
 
         var result = 1 - (dodgeFromConfig * dodgeFromDexterity);
-        _log.Verbose($"Total Dodge = {result:P2}");
+        if (result > 0)
+        {
+            _log.Verbose($"Total Dodge = {result:P2}");
+        }
 
         return result;
     }
